d697af0d7984487422ed589deb535d8c201c36d0
samiro <mohamedsamirovich@gmail.com>
2023-02-28 17:18:59 +0200

    first commitd

---
+++ resources/js/navigation/vertical/index.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    title: 'Home',
+    to: { name: 'index' },
+    icon: { icon: 'tabler-smart-home' },
+  },
+  {
+    title: 'Second page',
+    to: { name: 'second-page' },
+    icon: { icon: 'tabler-file' },
+  },
+  {
+    title: 'tab',
+    to: { name: 'tab' },
+    icon: { icon: 'tabler-file' },
+  },
+  {
+    title: 'user',
+    to: { name: 'users' },
+    icon: { icon: 'tabler-file' },
+  },
+  {
+    title: 'Floors',
+    to: { name: 'floors' },
+    icon: { icon: 'tabler-file' },
+  },
+  {
+    title: 'Rooms',
+    to: { name: 'rooms' },
+    icon: { icon: 'tabler-file' },
+  },
+  {
+    title: 'Room Type',
+    to: { name: 'room_type' },
+    icon: { icon: 'tabler-file' },
+  },
+  {
+    title: 'permession Data',
+    to: { name: 'Permission_Table' },
+    icon: { icon: 'tabler-file' },
+  },
+  {
+    title: 'permession',
+    to: { name: 'permession' },
+    icon: { icon: 'tabler-file' },
+  },
+  {
+    title: 'roles',
+    to: { name: 'roles' },
+    icon: { icon: 'tabler-file' },
+  },
+  {
+    title: 'Feature',
+    to: { name: 'feature' },
+    icon: { icon: 'tabler-file' },
+  },
+
+]
